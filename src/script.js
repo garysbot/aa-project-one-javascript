76f346de6b3fb3310005c2a5f89ed010963da105
Gary <g@modernhuman.xyz>
2023-10-24 09:23:23 -0400

    Update folder structure for Github Pages

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
+import * as THREE from '/node_modules/three/build/three.module.js';
+import { PointerLockControls } from '/node_modules/three/examples/jsm/controls/PointerLockControls.js';
 
 // ! Setup ----------------------------------------------------------------------------------
 // * Move to Experience class
